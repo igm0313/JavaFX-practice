106384875de47ccc8a1d0a8fab1906052b79fd9b
igm0313 <igm0313.handong.ac.kr>
2026-04-16 23:48:40 +0900

    make easy

---
+++ src/main/java/arg.java
@@ -0,0 +1,11 @@
+public class arg {
+    public static void main(String[] args){
+        int n = 3;
+        do{
+            n++;
+            System.out.println("n= "+n);
+
+        }
+        while(n<10);
+    }
+}
--- src/main/java/fx2.java
+++ src/main/java/fx2.java
@@ -27,23 +27,19 @@ public class fx2 extends Application { //부모클래스 Application을 받아�
         Canvas canvas = new Canvas(400, 300); // 새로운 캔버스 생성
         GraphicsContext gc = canvas.getGraphicsContext2D(); //캔버스에 그림그리는 붓이라고 생각
         gc.setFill(Color.YELLOW);
-        gc.rect(100,50,200,200); //사각형 그리기
-        gc.setFill(Color.YELLOW);
         gc.fillOval(100, 50, 200, 200); //빈 원 그리기
         gc.setFill(Color.BLACK);
         gc.fillOval(155, 100, 10, 20); //속이 찬 원 그리기
-        gc.setFill(Color.BLACK);
         gc.fillOval(230, 100, 10, 20); //속이 찬 원
-        gc.setFill(Color.YELLOW);
         gc.strokeArc(150, 160, 100, 50, 180, 180, ArcType.OPEN); // 호 그리기
 
-        gc.setFill(Color.BLACK);
         gc.setFont(Font.font("Times New Roman",30));
         gc.fillText("Hello!",300,40);
 
         root.getChildren().add(canvas); //캔버스를 그룹안에 넣음
         primaryStage.setTitle("Happy face in JavaFX"); //프로그램 이름
         primaryStage.setScene(scene); //stage의 scene 추가
+        primaryStage.sizeToScene();
         primaryStage.show(); //stage 보여줌
     }
 }
